20de24ad2403f6110d3fcc3f3062a614593a9103
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-02-13 12:12:50 +0900

    ステップ3(購入）の途中まで

--- sample.rb
+++ sample.rb
@@ -36,31 +36,75 @@ class VendingMachine
   end
 end
 
+
 class Juice_management
+# ステップ２　ジュースの管理
 
   def juice_name
     @juice1 = { name: "cola", price: 120, number: 5 }
-    puts @juice1.values_at(:name, :price, :number)
   end
 
 end
 
-vm = Juice_management.new
-vm.juice_name
 
+class Purchase
+# ステップ３　購入(Purchase)
 
+  def maney_and_stock_judge(money, number)
 
+    # 投入金額が120円以上かつ、コーラの本数が0本より大きい場合
+    if money >= 120 && number > 0
+      # 購入本数を入力
+      puts "何本買うか入力してください"
+      purchase_number =  gets.to_i
 
+      # 在庫が0未満の場合、または投入額より購入額が多い場合は再入力
+      if (number - purchase_number) < 0 || (money - 120 * purchase_number) < 0
+        puts "本数を減らしてください"
+        maney_and_stock_judge(money, number)
+      end
+
+      # 現在の売り上げ金額
+      current_sales_amount = 120 * purchase_number
+
+      # 在庫
+      stock = number - purchase_number
+
+      # 釣り銭
+      change = money - 120 * purchase_number
+
+      puts "現在の売り上げ金額：#{current_sales_amount}、在庫：#{stock}本、釣り銭：#{change}円"
+
+      # number -= purchase_number
+
+    else
+
+    end
+
+  end
+
+end
 
 vm = VendingMachine.new
+jm = Juice_management.new
+pc = Purchase.new
 
 # 作成した自動販売機に100円を入れる
-vm.slot_money (100)
+# vm.slot_money (100)
 
 # 作成した自動販売機に入れたお金がいくらかを確認する（表示する）
 vm.current_slot_money
 
+# ジュースの情報を表示する(ステップ2)
+puts "コーラの情報"
+puts jm.juice_name.values_at(:name, :price, :number)
+
+# ジュースの購入（ステップ3）
+puts "100円を入れた時の挙動、コーラの残りの本数、10, 50, 100, 500, 1000しか入らない"
+pc.maney_and_stock_judge(vm.slot_money(500), jm.juice_name[:number])
+
 # 作成した自動販売機に入れたお金を返してもらう
+puts "作成した自動販売機に入れたお金を返してもらう"
 vm.return_money
 
-# $irbをして、require '/Users/doda_hironobu/vending_machine/sample.rb'と入力して実行してみよう
+# $irbをして、次のように入力して実行してみよう。require '/Users/doda_hironobu/vending_machine/sample.rb'
