3a650e99e8b3010fae832c4cc60e3d9863e3f75b
shotasan <wondermoai@gmail.com>
2019-03-25 16:13:49 +0900

    投入金額が100円に満たないまま購入処理に進んだ場合に、再度金額の投入処理を行えるように修正。Juice_managementクラスのinitializeを修正。

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,18 +1,17 @@
 class VendingMachine
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   MONEY = [10, 50, 100, 500, 1000].freeze
-
-  #（自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
+  
   # 最初の自動販売機に入っている金額は0円
   def initialize
     @slot_money = 0
   end
-
+  
   # 自動販売機に入っているお金を表示するメソッド
   def current_slot_money
     puts "現在の投入金額は#{@slot_money}円です"
   end
-
+  
   # お金を投入するメソッド
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   # 投入は複数回できる。
@@ -29,11 +28,11 @@ class VendingMachine
     elsif money == 0
       return_money
       exit
-
+      
       # 購入処理に進む場合
     elsif money == 1
       return @slot_money
-
+      
       # その他の入力が行われた場合
     else
       puts "使えないお金なので返却します。"
@@ -42,7 +41,7 @@ class VendingMachine
       slot_money(gets.to_i)
     end
   end
-
+  
   # 購入可能なドリンクリストを表示するメソッド
   def purchasable_drinks
     if @slot_money >= 200
@@ -51,9 +50,12 @@ class VendingMachine
       puts "『コーラ、水』が買えます。"
     elsif @slot_money >= 100
       puts "『水』が買えます。"
+    else
+      puts "お金が足りません。再度お金[10,50,100,500,1000円]を入力してください。"
+      slot_money(gets.to_i)
     end
   end
-
+  
   # 返すお金の金額を表示するするメソッド
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力し、０円に戻す
   def return_money
@@ -64,30 +66,38 @@ end
 
 
 class Juice_management
-  def juice_name
+
+  def initialize
     @juice1 = { name: "コーラ", price: 120, number: 5 }
     @juice2 = { name: "レッドブル", price: 200, number: 5 }
     @juice3 = { name: "水", price: 100, number: 5 }
-    juices = [@juice1, @juice2, @juice3]
-    juices.each do |juice|
+
+    @current_sales_amount = 0
+  end
+  
+  # ドリンクの情報を出力するメソッド
+  def juice_name
+    @juices = [@juice1, @juice2, @juice3]
+    @juices.each do |juice|
       puts "- #{juice.values[0]} (値段:#{juice.values[1]}円,  在庫:#{juice.values[2]})"
     end
   end
 
   # どのドリンクを買うか選択させるメソッド
+  # 在庫（juice[:number])が0だと選択が表示されなくなる
   def purchase_selection
-    # 在庫（juice[:number])が0だと選択が表示されなくなる
+
     puts "購入したいジュースの番号を入力してください。"
     if @juice1[:number] > 0
-      puts "1: コーラ(#{@juice1[:price]}円)"
+      puts "1: #{@juice1[:name]}(#{@juice1[:price]}円)"
     end
 
     if @juice2[:number] > 0
-      puts "2: レッドブル(#{@juice2[:price]}円)"
+      puts "2: #{@juice2[:name]}(#{@juice2[:price]}円)"
     end
 
     if @juice3[:number] > 0
-      puts "3: 水(#{@juice3[:price]}円)"
+      puts "3: #{@juice3[:name]}(#{@juice3[:price]}円)"
     end
 
     if @change
@@ -113,10 +123,6 @@ class Juice_management
         puts "項目の番号から選択してください。"
         purchase_selection
     end
-  end
-
-  def initialize
-    @current_sales_amount = 0
   end
 
   # 投入金額、在庫の点で購入できるか判定するメソッド
@@ -152,16 +158,17 @@ class Juice_management
 end
 
 vm = VendingMachine.new
+
 jm = Juice_management.new
 
 # 1.ジュースの情報を表示する
 jm.juice_name
 
-# 2.お金の投入
+# 2.投入金額の合計をtotal_feeに代入
 puts "[10,50,100,500,1000円]を入力してください。"
 total_fee = vm.slot_money(gets.to_i)
 
-# 3.購入可能なドリンクリスト
+# 3.投入金額の合計から、購入可能なドリンクリストを表示する
 vm.purchasable_drinks
 
 # 4.購入するジュースの選択処理
